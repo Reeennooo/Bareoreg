b5b95657d23723c77b12d33ea5a910f146ce037d
Renat Kurkin <renat.kurkin@mail.ru>
2023-09-14 20:05:58 +0300

    Логика для вывода операций.
    ТРЕБУЕТ ДОРАБОТКИ

--- src/blocks/form-creating-operation/form-creating-operation.js
+++ src/blocks/form-creating-operation/form-creating-operation.js
@@ -10,6 +10,20 @@ const allRules = {
             message: 'Обязательное поле',
         },
     },
+    access: {
+        required: {
+            message: 'Обязательное поле',
+        },
+    },
+    'duration-operation': {
+        customRange: {
+            min: 40,
+            max: 120,
+        },
+        required: {
+            message: 'Обязательное поле',
+        },
+    },
     'weight-operation': {
         customRange: {
             min: 60,
@@ -54,16 +68,29 @@ document.addEventListener('DOMContentLoaded', () => {
         // inline: true,
     });
     new AirDatepicker('#calendar-discharge', {});
+    new AirDatepicker('#calendar-adjustment-bandage', {});
 
     // выпадающие списки
     new ItcCustomSelect('#surgeon');
     new ItcCustomSelect('#assistants');
-    new ItcCustomSelect('#type-of-operation');
+    const selectTypeOperation = new ItcCustomSelect('#type-of-operation');
     new ItcCustomSelect('#reason-for-revision');
-    new ItcCustomSelect('#kind-of-operation');
+    const selectKindOperation = new ItcCustomSelect('#kind-of-operation');
     new ItcCustomSelect('#access');
     new ItcCustomSelect('#pain-relief');
     new ItcCustomSelect('#simultaneous-operation');
+
+    // RYGB (Гастрошунтирование)
+    new ItcCustomSelect('#stitching-machine');
+    new ItcCustomSelect('#used-cassettes');
+    new ItcCustomSelect('#diameter-of-the-anastomosis');
+    new ItcCustomSelect('#manual-anastomosis');
+
+    // Внутрижелудочный баллон
+    new ItcCustomSelect('#ballon-type');
+
+    // Бандажирование желудка
+    new ItcCustomSelect('#type-of-bandage');
 
     // связи
     const hasConnections = document.querySelectorAll('[data-has-connection]');
@@ -86,7 +113,6 @@ document.addEventListener('DOMContentLoaded', () => {
                         element.checked = false;
                         element.setAttribute('disabled', 'disabled');
                         document.querySelector(`[data-connected=${element.dataset.hasConnection}]`).classList.remove('is-active');
-                        // document.querySelector(`[data-connected=${element.dataset.hasConnection}]`).style.display = 'none';
                     });
                 } else {
                     connectedEls.forEach((element) => {
@@ -103,14 +129,137 @@ document.addEventListener('DOMContentLoaded', () => {
                     const index = mutation.target.dataset.index;
                     if (index === '1' || index === '3') {
                         connectedEl.classList.add('is-active');
-                        // connectedEl.style.display = 'block';
                     } else {
                         connectedEl.classList.remove('is-active');
-                        // connectedEl.style.display = 'none';
                     }
                 }
             });
             observer.observe(button, { attributes: true, attributeFilter: ['data-index'] });
         }
+
+        // связи операций.
+        // Будем искать элементы которые привязаны к полю операция data-connected='operation'
+        // т.к таких элементов будет несколько, то нам придётся перебрать их
+        // и сделать активным только тот элемент у которого data-name === value активного элемента списка.
+        else if (hasConnectEl.dataset.hasConnection === 'operation') {
+            let connectedElements = document.querySelectorAll(`[data-connected=${hasConnectEl.dataset.hasConnection}]`);
+            // console.log(connectedElements);
+            let button = selectKindOperation._elToggle;
+            console.log(connectedElements);
+            let buttonObvserver = new MutationObserver(() => {
+                setOperationBlock(button, connectedElements);
+            });
+            buttonObvserver.observe(button, { attributeFilter: ['value'] });
+        }
     });
+
+    const observers = [];
+    function setOperationBlock(observationElement, connectedElements) {
+        const selectOperationHint = document.querySelector('.form-creating-operation__operation .group__info');
+        const selectedOperationName = observationElement.value;
+        const connectedGroupArr = [...connectedElements];
+        let operationNumber = 1;
+
+        connectedGroupArr.forEach((group) => group.classList.remove('is-active'));
+        const operationGroup = connectedGroupArr.find((group) => group.dataset.name === selectedOperationName && group.dataset.number === String(operationNumber));
+
+        if (!operationGroup) return;
+
+        console.log(operationGroup);
+        showOperationGroup(operationGroup);
+
+        function showOperationGroup(group) {
+            if (!group) return;
+
+            group.classList.add('is-active');
+            selectOperationHint.style.display = 'none';
+
+            if (group.classList.contains('is-filled')) {
+                operationNumber++;
+
+                const nextOperationGroup = connectedGroupArr.find((group) => group.dataset.name === selectedOperationName && group.dataset.number === String(operationNumber));
+                if (nextOperationGroup) {
+                    showOperationGroup(nextOperationGroup);
+                }
+                return;
+            }
+
+            createObserver(group);
+        }
+
+        function createObserver(observeredElement) {
+            const operationGroupObserver = new MutationObserver((mutations) => {
+                for (const mutation of mutations) {
+                    if (mutation.target.classList.contains('is-filled')) {
+                        operationNumber++;
+                        const nextOperationGroup = connectedGroupArr.find((group) => group.dataset.name === selectedOperationName && group.dataset.number === String(operationNumber));
+                        showOperationGroup(nextOperationGroup);
+                    }
+                }
+            });
+            operationGroupObserver.observe(observeredElement, { attributeFilter: ['class'] });
+            observers.push(operationGroupObserver);
+        }
+    }
+
+    // связи пунктов в различных select
+    const button = selectTypeOperation._elToggle;
+    // Опция операция ВЖБ
+    const connectedOption = selectTypeOperation._el.querySelector(`.itc-select__option[data-index='10']`);
+
+    if (button) {
+        let observer = new MutationObserver(() => {
+            if (selectTypeOperation.selectedIndex !== '0') {
+                connectedOption.classList.add('disabled');
+            } else {
+                connectedOption.classList.remove('disabled');
+            }
+        });
+        observer.observe(button, { attributeFilter: ['data-index'] });
+    }
+
+    // связи пунктов select и допустимых значений
+    const kindOperationBtn = selectKindOperation._elToggle;
+    if (kindOperationBtn) {
+        let observer = new MutationObserver(() => {
+            if (selectKindOperation.selectedIndex === '8') {
+                assignInputRules({
+                    'duration-operation': {
+                        customRange: {
+                            min: 10,
+                            max: 30,
+                        },
+                        required: {
+                            message: 'Обязательное поле',
+                        },
+                    },
+                });
+            } else {
+                assignInputRules({
+                    'duration-operation': {
+                        customRange: {
+                            min: 40,
+                            max: 120,
+                        },
+                        required: {
+                            message: 'Обязательное поле',
+                        },
+                    },
+                });
+            }
+            // setOperationBlock(selectKindOperation.value);
+        });
+        observer.observe(kindOperationBtn, { attributeFilter: ['data-index', 'value'] });
+
+        // операции
+        // const operationGroup = document.querySelector(`[data-group-name='operation']`);
+        // const operationSelectedTxt = selectKindOperation._textSelectedEl;
+        // function setOperationBlock(operationValue) {
+        //     let operationName = operationSelectedTxt.innerText;
+
+        //     console.log(operationValue);
+        //     console.log(operationName);
+        //     // operationGroup;
+        // }
+    }
 });
