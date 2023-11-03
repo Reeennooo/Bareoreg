75857df61fe686fe82b4424613973d048600eb99
Reeennooo <105305554+Reeennooo@users.noreply.github.com>
2023-11-03 14:46:08 +0300

    info circle fix

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -1,6 +1,20 @@
 import tippy from 'tippy.js';
 
-tippy('#city-info', {
-    content: 'Если игнорировать это поле, пациента не будет на географической карте',
-    theme: 'violet',
-});
+// tippy('#city-info', {
+//     content: 'Если игнорировать это поле, пациента не будет на географической карте',
+//     theme: 'violet',
+// });
+
+function initTippy() {
+    const infoCirles = document.querySelectorAll('.info');
+    infoCirles.forEach((circle) => {
+        let circleId = circle.getAttribute('id');
+        if (circleId && circle.dataset.info) {
+            console.log(circleId);
+            const infoObj = JSON.parse(circle.dataset.info);
+            tippy(`#${circleId}`, infoObj);
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', initTippy);
